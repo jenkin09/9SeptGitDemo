c596e6fa188e5947e131bd8ca6deb2528eebb4df
userone@gmail.com <madhupatil9876@gmail.com>
2024-02-26 14:58:47 +0530

    added more test class and page class

--- src/pages/Loginpage.java
+++ src/pages/Loginpage.java
@@ -1,5 +1,14 @@
 package pages;
 
 public class Loginpage {
-
+	
+	public void loginTOApp()
+	{
+		System.out.println("Login to application");
+	}
+	public void FetchTittle()
+	{
+		System.out.println("Getting tittle of page");
+	}
+	
 }
