759eae041139c36b9c10976ac29c4cb0e2ba4020
userone@gmail.com <madhupatil9876@gmail.com>
2024-02-26 18:58:39 +0530

    modified home page

--- src/pages/Homepage.java
+++ src/pages/Homepage.java
@@ -2,23 +2,12 @@ package pages;
 
 public class Homepage {
 
-<<<<<<< HEAD
+
 public void get title of page()
 
 {
    system.out.println("Title of page change by blue");
 }
 
-=======
-	
-	
-	public void getTitleOfPage()
-	{
-		System.out.println("Title of page edited by brown");
-	}
-	
-	
-	
-	
->>>>>>> 0f1701b29537fa91d9f560bc128037e9e3c0578a
+
 }
