b73c3165bedcb8d231e480d96611ab38bcc2086e
userone@gmail.com <madhupatil9876@gmail.com>
2024-02-26 18:01:57 +0530

    modified the login code

--- src/pages/Loginpage.java
+++ src/pages/Loginpage.java
@@ -5,10 +5,12 @@ public class Loginpage {
 	public void loginTOApp()
 	{
 		System.out.println("Login to application");
+		
+		System.out.println("Entering the username change by user 1");
 	}
 	public void FetchTittle()
 	{
-		System.out.println("Getting tittle of page");
+		System.out.println("Getting title of page");
 	}
 	
 }
